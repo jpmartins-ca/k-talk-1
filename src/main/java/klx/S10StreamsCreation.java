9c1da3d7a237cdf09c77247214fd6a37b3610566
Joao PAULO MARTINS <joao-paulo.martins-prestataire@ca-ps.com>
2025-10-27 15:16:58 +0000

    chore: minor code style improvements

--- src/main/java/klx/S10StreamsCreation.java
+++ src/main/java/klx/S10StreamsCreation.java
@@ -6,10 +6,11 @@ import java.util.stream.Stream;
 public class S10StreamsCreation {
 
     public static void main(String[] args){
-        Stream.generate (Math::random). limit(2).forEach(System.out::println);
-        Stream.iterate(0, n -> n + 2).limit(2) .forEach(System.out::println);
+        Stream.generate(Math::random).limit(2).forEach(System.out::println);
+        Stream.iterate(0,n->n+2).limit(2).forEach(System.out::println);
         //for(;;){System.out.println(Math.random());}
-        Stream. generate (Math::random).forEach(System.out::println);
+        Stream.generate(Math::random).forEach(System.out::println);
+        // Non-Short-Circuit Consumes infinite Stream
     }
 
 }
